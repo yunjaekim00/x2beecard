1dc209523d84277244d21a5109e1208a13f34f93
yunjaekim <yunjaekim007@gmail.com>
2023-11-26 18:08:05 +0900

    card update

--- bin/cli.js
+++ bin/cli.js
@@ -5,9 +5,7 @@ const https = require('https');
 const path = require('path');
 
 // The URL where your button component is hosted
-const BASE_COMPONENT_URL = 'https://github.com/yunjaekim00/x2beecard/tree/99085ecce126d7395d6baaca0b405737fed90cf2/ui';
-
-
+const BASE_COMPONENT_URL = 'https://raw.githubusercontent.com/yunjaekim00/x2beecard/main/ui';
 
 // The path to the current working directory where the command is executed
 const localDirPath = process.cwd();
